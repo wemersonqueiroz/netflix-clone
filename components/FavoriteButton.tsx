e574683718249394b067206f9f253a365e4b29fa
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2023-03-21 09:32:59 +0000

    final

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -11,6 +11,7 @@ interface FavoriteButtonProps {
 
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
   const { mutate: mutateFavorites } = useFavorites()
+
   const { data: currentUser, mutate } = useCurrentUser()
 
   const isFavorite = useMemo(() => {
@@ -21,18 +22,19 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
 
   const toggleFavorites = useCallback(async () => {
     let response
+
     if (isFavorite) {
       response = await axios.delete("/api/favorite", { data: { movieId } })
+    } else {
+      response = await axios.post("/api/favorite", { movieId })
     }
-    response = await axios.post("/api/favorite", { movieId })
 
-    const updatedFavoriteIds = response.data.favoriteIds
+    const updatedFavoriteIds = response?.data?.favoriteIds
 
     mutate({
       ...currentUser,
       favoriteIds: updatedFavoriteIds,
     })
-
     mutateFavorites()
   }, [movieId, isFavorite, currentUser, mutate, mutateFavorites])
 
@@ -41,8 +43,12 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
   return (
     <div
       onClick={toggleFavorites}
-      className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex items-center justify-center transition hover:border-neutral-300">
-      <Icon className="text-white" size={25} />
+      className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+      <AiOutlinePlus
+        className={`text-white group-hover/item:text-neutral-300 w-4 lg:w-6 duration-300 ${
+          isFavorite ? "rotate-45 text-red-500" : ""
+        }`}
+      />
     </div>
   )
 }
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,15 @@
+import useInfoModal from "@/hooks/useInfoModal"
 import { useRouter } from "next/router"
 import React from "react"
 import { BsFillPlayFill } from "react-icons/bs"
+import { BiChevronDown } from "react-icons/bi"
 import FavoriteButton from "./FavoriteButton"
 interface MovieCartProps {
   data: Record<string, any>
 }
 
 const MovieCard: React.FC<MovieCartProps> = ({ data }) => {
+  const { openModal } = useInfoModal()
   const router = useRouter()
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -29,6 +32,14 @@ const MovieCard: React.FC<MovieCartProps> = ({ data }) => {
               <BsFillPlayFill size={20} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+              <BiChevronDown
+                className="text-white group-hover/item:text-neutral-300"
+                size={30}
+              />
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
